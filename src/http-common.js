275fb3fe9f884da988e7ab1d37a0bb26575ca17c
Alessandro Morita <alessandro.morita@gmail.com>
2020-09-04 11:55:46 -0300

    alterando variáveis de conexão

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'amm-grades-api.herokuapp.com:8081',
+  baseURL: process.env.AXIOS_HOST,
   headers: {
     'Content-type': 'application/json',
   },
